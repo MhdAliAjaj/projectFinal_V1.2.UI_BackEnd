7c292469b7d744c121f51cff01393566ee7e2d4b
Mhd Ali Ajaj <163741790+MhdAliAjaj@users.noreply.github.com>
2024-06-08 11:07:18 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,16 @@
                                 </li>
                             @endif
                         @else
+<<<<<<< Updated upstream
+                        @canany(['create-user', 'edit-user', 'delete-user'])
+                        <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+                    @endcanany
+                    @canany(['create-category', 'edit-category', 'delete-category'])
+                        <li><a class="nav-link" href="{{ route('category.index') }}">Manage category</a></li>
+                    @endcanany
+
+=======
+>>>>>>> Stashed changes
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
