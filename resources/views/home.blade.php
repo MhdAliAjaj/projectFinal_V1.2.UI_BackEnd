0266a6e26fbedc402d56196905a0175c947fd306
Mhd Ali Ajaj <163741790+MhdAliAjaj@users.noreply.github.com>
2024-06-23 18:32:09 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,6 +26,15 @@
                         <i class="bi bi-people"></i> Manage category</a>
                         @endcanany
 
+                    @canany(['create-service', 'edit-service', 'delete-service'])
+                        <a class="btn btn-success" href="{{ route('services.index') }}">
+                        <i class="bi bi-people"></i> Manage Services</a>
+                    @endcanany
+                    @if(auth()->user()->hasRole('Admin'))
+                    <a class="btn btn-success" href="{{ route('reports.index') }}">Order Reports</a>
+                    <a class="btn btn-success" href="{{ route('reports.annual') }}">Overall Performance</a>
+                    @endif
+
 
                 </div>
             </div>
