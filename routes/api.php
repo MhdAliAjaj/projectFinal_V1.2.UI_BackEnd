7c859d9399858ccf285474cb2d2267ced00eeeb4
JChr15t <yousefaaaammar.com>
2024-06-25 08:40:39 +0300

    Adding Service Request

--- routes/api.php
+++ routes/api.php
@@ -5,40 +5,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\ServiceController;
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\Api\CompanyInfoController;
->>>>>>> e8c5f1935990507f1d204e619f21bd4422eddded
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\CompanyInfoController;
+use App\Http\Controllers\Api\ServiceRequestController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/services', [ServiceController::class, 'index']);
-Route::get('/services/{service}', [ServiceController::class,'show']);
+Route::get('/services/{service}', [ServiceController::class, 'show']);
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-
-<<<<<<< HEAD
-=======
-
->>>>>>> e8c5f1935990507f1d204e619f21bd4422eddded
-
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
 });
-// Route::post()
-Route::get('/service',[ServiceController::class,'index']);
-Route::get('/information',[CompanyInfoController::class,'info']);
+Route::post('/service-requests', [ServiceRequestController::class,'create']);
